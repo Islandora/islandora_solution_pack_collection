274d9a179307d1674b5ac1685a3ccc27279b38a3
DannyJoris <danny_joris@yahoo.co.uk>
2012-09-11 14:18:52 -0300

    coder cleanup

--- theme/islandora-basic-collection-grid.tpl.php
+++ theme/islandora-basic-collection-grid.tpl.php
@@ -1,27 +1,12 @@
 <?php
 
-/*
+/**
+ * @file
  * islandora-basic-collection.tpl.php
- * 
  *
- * 
- * This file is part of Islandora.
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with the program.  If not, see <http ://www.gnu.org/licenses/>.
+ * @TODO: needs documentation about file and variables
  */
 ?>
-
 
 <div class="islandora islandora-basic-collection">
   <div class="islandora-basic-collection-grid clearfix">
