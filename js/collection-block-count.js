70e710d54f982cd1a5003fb30d9687efc544cdd0
Adam Vessey <adam-vessey@users.noreply.github.com>
2014-12-11 10:35:05 -0400

    Bit safer in the Javascript...
    Avoid potentially calling AJAX things multiple times.

--- js/collection-block-count.js
+++ js/collection-block-count.js
@@ -1,11 +1,13 @@
 (function ($) {
   Drupal.behaviors.islandora_basic_collection_count_block = {
     attach: function (context, settings) {
-      $.ajax({
-        url: settings.islandora_basic_collection.count_block.callback,
-        success: function (data, textStatus, jqXHR) {
-          $('span#' + settings.islandora_basic_collection.count_block.id, context).html(data);
-        }
+      $('span#' + settings.islandora_basic_collection.count_block.id, context).once('islandora_basic_collection_count_block', function() {
+        $.ajax({
+          url: settings.islandora_basic_collection.count_block.callback,
+          success: function (data, textStatus, jqXHR) {
+            $('span#' + settings.islandora_basic_collection.count_block.id, context).html(data);
+          }
+        });
       });
     }
   }
