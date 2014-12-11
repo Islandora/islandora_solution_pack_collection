0c3e7f448d7718d86a03a0accdf36048a50c2d04
Adam Vessey <adam-vessey@users.noreply.github.com>
2014-12-11 10:58:08 -0400

    Use passed along element.

--- js/collection-block-count.js
+++ js/collection-block-count.js
@@ -2,10 +2,11 @@
   Drupal.behaviors.islandora_basic_collection_count_block = {
     attach: function (context, settings) {
       $('span#' + settings.islandora_basic_collection.count_block.id, context).once('islandora_basic_collection_count_block', function() {
+        var that = this;
         $.ajax({
           url: settings.islandora_basic_collection.count_block.callback,
           success: function (data, textStatus, jqXHR) {
-            $('span#' + settings.islandora_basic_collection.count_block.id, context).html(data);
+            $(that).html(data);
           }
         });
       });
