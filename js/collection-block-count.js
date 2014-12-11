f302f2a1866fcbc8ba5f9c2ee2f531be7ef1a796
Adam Vessey <adam-vessey@users.noreply.github.com>
2014-12-11 13:41:14 -0400

    Drop scope hack, 'cause jQuery.ajax() has a parameter for... "that". :V

--- js/collection-block-count.js
+++ js/collection-block-count.js
@@ -2,12 +2,12 @@
   Drupal.behaviors.islandora_basic_collection_count_block = {
     attach: function (context, settings) {
       $('span#' + settings.islandora_basic_collection.count_block.id, context).once('islandora_basic_collection_count_block', function() {
-        var that = this;
         $.ajax({
           url: settings.islandora_basic_collection.count_block.callback,
           success: function (data, textStatus, jqXHR) {
-            $(that).html(data);
-          }
+            $(this).html(data);
+          },
+          context: this
         });
       });
     }
